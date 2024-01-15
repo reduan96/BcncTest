2cbd68d16aaec551ed8a732d3002803dbaa255f9
reduan96 <redu1996@gmail.com>
2024-01-15 21:14:37 +0100

    build(project): project structure added

---
+++ src/test/java/com/bcnc/BcncTest/BcncTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcnc.BcncTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcncTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
