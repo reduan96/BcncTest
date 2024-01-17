f3ba3729a092ad9191089f13dbdab49ece8b26d9
reduan96 <redu1996@gmail.com>
2024-01-17 22:19:18 +0100

    fix(domain): some changes at the entity and db added

--- src/main/java/com/bcnc/BcncTest/domain/entities/Prices.java
+++ src/main/java/com/bcnc/BcncTest/domain/entities/Prices.java
@@ -1,5 +1,8 @@
 package com.bcnc.BcncTest.domain.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 import java.time.LocalDateTime;
 
@@ -8,7 +11,13 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "PRICES")
 public class Prices {
+
+    @Id
+    private short pricesId;
+
     private short brandId;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
