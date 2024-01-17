0c8a98ff35c9f70342b67d73fd2dd17430e3040f
reduan96 <redu1996@gmail.com>
2024-01-17 18:01:47 +0100

    fix(infrastructure): some few changes

--- src/main/java/com/bcnc/BcncTest/infrastructure/inputport/PricesInputPort.java
+++ src/main/java/com/bcnc/BcncTest/infrastructure/inputport/PricesInputPort.java
@@ -1,9 +1,10 @@
 package com.bcnc.BcncTest.infrastructure.inputport;
 
-import com.bcnc.BcncTest.domain.Prices;
+import com.bcnc.BcncTest.domain.DTOs.PricesResult;
+
 import java.time.LocalDateTime;
 
 public interface PricesInputPort {
 
-    Prices getAppliedPrice(LocalDateTime startDate, int productId, short brandId);
+    PricesResult getAppliedPrice(LocalDateTime startDate, int productId, short brandId);
 }
--- src/main/java/com/bcnc/BcncTest/infrastructure/outportport/PricesRepository.java
+++ src/main/java/com/bcnc/BcncTest/infrastructure/outportport/PricesRepository.java
@@ -1,10 +1,10 @@
 package com.bcnc.BcncTest.infrastructure.outportport;
 
-import com.bcnc.BcncTest.domain.Prices;
+import com.bcnc.BcncTest.domain.DTOs.PricesResult;
 
 import java.time.LocalDateTime;
 
 public interface PricesRepository {
 
-    public Prices getAppliedPrice(LocalDateTime startDate, int productId, short brandId);
+    public PricesResult getAppliedPrice(LocalDateTime startDate, int productId, short brandId);
 }
