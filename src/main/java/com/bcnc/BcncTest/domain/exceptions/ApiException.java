6ead403be7e744d9dfabe9a516fd5e1537ab1da9
reduan96 <redu1996@gmail.com>
2024-01-18 23:58:24 +0100

    feat(domain): ApiException added

---
+++ src/main/java/com/bcnc/BcncTest/domain/exceptions/ApiException.java
@@ -0,0 +1,16 @@
+package com.bcnc.BcncTest.domain.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ApiException {
+
+    private final String message;
+    private final HttpStatus httpStatus;
+    private final ZonedDateTime timestamp;
+}
